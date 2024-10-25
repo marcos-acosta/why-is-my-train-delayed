419df20ac88e3eeb5f0c9be07aed9bd4b9249c37
Marcos Acosta <macosta1625@gmail.com>
2024-10-25 15:25:34 -0700

    Escape quotation marks and apostrophes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,16 +30,16 @@ export default function Home() {
         <div className={styles.blogParagraph}>
           Cursing the NYCT for unexpected delays is a favorite pastime for New
           Yorkers (no citation needed). After having gotten used to them myself,
-          I stopped wondering *why* these disruptions happen; "old
-          infrastructure" is an easy explanation. But this leaves a lot of
+          I stopped wondering *why* these disruptions happen; &quot;old
+          infrastructure&quot; is an easy explanation. But this leaves a lot of
           questions: What kind of infrastructure is faulty? Are the delays
           really mostly due to mechanical problems? Are certain lines, boroughs,
-          or stations more prone to delays? What could be done about it? Let's
-          take a look!
+          or stations more prone to delays? What could be done about it?
+          Let&apos;s take a look!
         </div>
         <div className={styles.blogParagraph}>
-          While there doesn't exist a "pre-made" dataset listing the cause of
-          each delay, we do have the{" "}
+          While there doesn&apos;t exist a &quot;pre-made&quot; dataset listing
+          the cause of each delay, we do have the{" "}
           <a
             href="https://data.ny.gov/Transportation/MTA-Service-Alerts-Beginning-April-2020/7kct-peq7/about_data"
             target="_blank"
@@ -51,20 +51,20 @@ export default function Home() {
           etc. Using a bit of basic natural language processing, we can extract
           information like the most likely cause of the delay and which train
           station the issue occurred at. The data preparation was actually very
-          fun and involved some tricky problems (like figuring out which "23 St"
-          is being referred to), so check out the <a>GitHub</a> to see the
-          detailed walk-through!
+          fun and involved some tricky problems (like figuring out which
+          &quot;23 St&quot; is being referred to), so check out the{" "}
+          <a>GitHub</a> to see the detailed walk-through!
         </div>
         <div className={styles.blogParagraph}>
           Now, we should make note that slight delays happen all the time, and
-          the MTA doesn't always send out an alert every time. But by analyzing
-          the delays which <i>do</i> generate alerts, we can inspect the (more
-          relevant) subset of delays which are big enough for subway riders to
-          care.
+          the MTA doesn&apos;t always send out an alert every time. But by
+          analyzing the delays which <i>do</i> generate alerts, we can inspect
+          the (more relevant) subset of delays which are big enough for subway
+          riders to care.
         </div>
         <div className={styles.blogParagraph}>
-          With that said, let's start simple and take a look at the distribution
-          of issues across all alerts since April 2020.
+          With that said, let&apos;s start simple and take a look at the
+          distribution of issues across all alerts since April 2020.
         </div>
         <IssueDistribution />
         <div className={styles.blogParagraph}>
@@ -72,18 +72,19 @@ export default function Home() {
           <b>issues caused by individuals</b> (aggression, medical attention,
           etc.) <b>account for a large proportion of train delays!</b> We can
           see this another way, by grouping multiple causes together under
-          "umbrella" issues:
+          &quot;umbrella&quot; issues:
         </div>
         <IssueTypeDistribution />
         <div className={styles.blogParagraph}>
           This is interesting, but this view is a little too high-level to gain
           much meaningful insight. Our next intuition would lead us to ask,
-          which trains are especially delay-prone? It wouldn't be very
+          which trains are especially delay-prone? It wouldn&apos;t be very
           interesting to just sum the number of alerts for each service, since
-          "busier" trains (e.g. the A) will naturally get more delay alerts than
-          "less busy" routes (e.g. the G). As a proxy for "business", we can use
-          the <b>planned number of trains delivered</b> for each service, which
-          is available through{" "}
+          &quot;busier&quot; trains (e.g. the A) will naturally get more delay
+          alerts than &quot;less busy&quot; routes (e.g. the G). As a proxy for
+          &quot;business&quot;, we can use the{" "}
+          <b>planned number of trains delivered</b> for each service, which is
+          available through{" "}
           <a
             href="https://data.ny.gov/Transportation/MTA-Subway-Service-Delivered-Beginning-2020/bg59-42xi/about_data"
             target="_blank"
